606a3e8e24f85ab76216b198b4dd6c097082757d
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-09-24 18:43:15 +0300

    test

--- 02_EXERCISE_data_types_and_variables/gladiator_expenses.py
+++ 02_EXERCISE_data_types_and_variables/gladiator_expenses.py
@@ -1,2 +1,24 @@
-name = 'test'
-name = 'test'
+lost_fights_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shild_price = float(input())
+armor_price = float(input())
+
+helmet_repair = 0
+sword_repair = 0
+shild_repair = 0
+
+for fight in range(1, lost_fights_count + 1):
+    if fight % 2 == 0:
+        helmet_repair += 1
+
+    if fight % 3 == 0:
+        sword_repair += 1
+
+    if fight % 2 == 0 and fight % 3 == 0:
+        shild_repair += 1
+
+armor_repair = shild_repair // 2
+total_sum = (helmet_repair * helmet_price) + (sword_repair * sword_price) + (shild_repair * shild_price) + (
+            armor_repair * armor_price)
+print(f"Gladiator expenses: {total_sum:.2f} aureus")
