3218bf6220395f594d35c46c08b935cc20b2c381
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-09-29 22:06:54 +0300

    Exercise - 29092023

--- 03_LAB_lists_basics/numbers_filter.py
+++ 03_LAB_lists_basics/numbers_filter.py
@@ -30,3 +30,26 @@ elif command == 'positive':
             filtered_string.append(current_num)
 
 print(filtered_string)
+
+# n = int(input())
+# COMMAND_EVEN = 'even'
+# COMMAND_ODD = 'odd'
+# COMMAND_NEGATIVE = 'negative'
+# COMMAND_POSITIVE = 'positive'
+#
+# numbers = [int(input()) for _ in range(n)]
+# filtered_numbers = []
+#
+# command = input()
+#
+# for num in numbers:
+#     filtered_command = ((command == COMMAND_EVEN and num % 2 == 0) or
+#                         (command == COMMAND_ODD and num % 2 != 0) or
+#                         (command == COMMAND_POSITIVE and num >= 0) or
+#                         (command == COMMAND_NEGATIVE and num < 0)
+#     )
+#
+#     if filtered_command:
+#         filtered_numbers.append(num)
+#
+# print(filtered_numbers)
