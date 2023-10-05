7067878912aa97b014886926e4d597f47eaa5bd3
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-10-05 20:19:26 +0300

    Exercise - 05102023

--- 04_LAB_Funtions/calculations.py
+++ 04_LAB_Funtions/calculations.py
@@ -1,12 +1,21 @@
 def calculator(action, num_a, num_b):
-    if action == 'multiply':
-        return num_a * num_b
-    elif action == 'divide':
-        return num_a / num_b
-    elif action == 'add':
-        return num_a + num_b
-    elif action == 'subtract':
-        return num_a - num_b
+    """
+    Calculate - multiply, divide, add, subtract { '*', '/', '+', '-' }
+    """
+    return {
+        'multiply': num_a * num_b,
+        'divide': int(num_a / num_b),
+        'add': num_a + num_b,
+        'subtract': num_a - num_b
+    }.get(action)
+    # if action == 'multiply':
+    #     return num_a * num_b
+    # elif action == 'divide':
+    #     return num_a / num_b
+    # elif action == 'add':
+    #     return num_a + num_b
+    # elif action == 'subtract':
+    #     return num_a - num_b
 
 
 operator = input()
