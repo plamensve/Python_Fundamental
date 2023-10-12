fea6ce9e838569e07d425148255bb737719d595c
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-10-12 19:09:10 +0300

    Exercise - 12102023

---
+++ 05_Lab_Lists_Advanced/palindrome_strings.py
@@ -0,0 +1,16 @@
+words_in_string = input().split()
+palindrome_word_count = input()
+
+palindrome_list = []
+counter = 0
+for word in words_in_string:
+    if word == word[::-1]:
+        palindrome_list.append(word)
+        if word == palindrome_word_count:
+            counter += 1
+
+print(palindrome_list)
+print(f"Found palindrome {counter} times")
+
+
+
