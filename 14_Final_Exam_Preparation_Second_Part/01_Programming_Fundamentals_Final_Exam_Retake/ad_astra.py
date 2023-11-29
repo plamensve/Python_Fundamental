203eede3b6069f44dedad085cc4b140be449f62e
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-11-29 10:59:55 +0200

    Exercise - 28112023

---
+++ 14_Final_Exam_Preparation_Second_Part/01_Programming_Fundamentals_Final_Exam_Retake/ad_astra.py
@@ -0,0 +1,17 @@
+import re
+
+text_string = input()
+
+pattern = r'(?i)([#|])([a-z\s]+)(\1)(\d{2}\/\d{2}\/\d{2})(\1)([0-9]+)(\1)'
+matches = re.findall(pattern, text_string)
+
+total_calories = 0
+for match in matches:
+    total_calories += int(match[5])
+print(f"You have food to last you for: {total_calories // 2000} days!")
+
+for match in matches:
+    item = match[1]
+    best_before = match[3]
+    calories = match[5]
+    print(f"Item: {item}, Best before: {best_before}, Nutrition: {calories}")
