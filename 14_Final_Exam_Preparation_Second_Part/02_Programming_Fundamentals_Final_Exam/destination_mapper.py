d3a8621a76c174d83148337642ac505628d3bc15
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-11-30 18:09:51 +0200

    Exercise - 29112023

---
+++ 14_Final_Exam_Preparation_Second_Part/02_Programming_Fundamentals_Final_Exam/destination_mapper.py
@@ -0,0 +1,15 @@
+import re
+
+places = input()
+
+pattern = r'([=|\/])([A-Z][A-Za-z]{2,})\1'
+# (=|/)([A-Z][A-Za-z]{2,})\1
+matches = re.findall(pattern, places)
+
+lengths_sum = 0
+destinations = []
+for match in matches:
+    lengths_sum += int(len(match[1]))
+    destinations.append(match[1])
+
+print(f"Destinations: {', '.join(destinations)}\nTravel Points: {lengths_sum}")
---
+++ 14_Final_Exam_Preparation_Second_Part/02_Programming_Fundamentals_Final_Exam/test_.py
@@ -0,0 +1,3 @@
+cm = int(input())
+inch = cm * 2.54
+print(inch)
