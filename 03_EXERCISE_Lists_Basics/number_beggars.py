60225086f3ec8c9f67190fbb5433188ba6d6c033
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-12-02 02:41:46 +0200

    Exercise - 02122023

--- 03_EXERCISE_Lists_Basics/number_beggars.py
+++ 03_EXERCISE_Lists_Basics/number_beggars.py
@@ -4,15 +4,15 @@ money_as_integer = []
 
 for current_money in money_as_string:
     money_as_integer.append(int(current_money))
-starting_intex = 0
+starting_index = 0
 final_sum = []
 
-while starting_intex < number_of_beggars:
+while starting_index < number_of_beggars:
     current_beggar_sum = 0
 
-    for current_index in range(starting_intex, len(money_as_integer), number_of_beggars):
+    for current_index in range(starting_index, len(money_as_integer), number_of_beggars):
         current_beggar_sum += money_as_integer[current_index]
     final_sum.append(current_beggar_sum)
-    starting_intex += 1
+    starting_index += 1
 
 print(final_sum)
